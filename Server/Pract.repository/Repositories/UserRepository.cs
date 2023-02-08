5df5674c2a6d8c564f5ab26b8a3d8c7463956647
tamar76 <t76208@gmail.com>
2023-02-09 00:19:51 +0200

    Server code

--- Server/Pract.repository/Repositories/UserRepository.cs
+++ Server/Pract.repository/Repositories/UserRepository.cs
@@ -30,7 +30,7 @@ namespace Pract.Repository.Repositories
             return news;
         }
 
-       
+     
    
     }
 }
---
+++ Server/Pract.services/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Pract.common.DTO_s;
+using Pract.Repository.Entities;
+using Pract.Repository.Interfaces;
+using Pract.Repository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract.Services.Entities
+{
+    public class UserService : IService<UserDTO>
+    {
+        private readonly IMapper _mapper;
+        private readonly IDataRepository<User> _UserRepository;
+        public UserService(IMapper mapper, IDataRepository<User> childRepository)
+        {
+            _mapper = mapper;
+            _UserRepository = childRepository;
+        }
+
+        public async Task<UserDTO> AddAsync(List<UserDTO> entity)
+        {
+            return _mapper.Map<UserDTO>(await _UserRepository.AddAsync(_mapper.Map<User>(entity)));
+        }
+     
+    }
+}
