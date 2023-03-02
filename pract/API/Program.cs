c28c030d10d5b251214ace3fd128543bf3811624
tamar76 <t76208@gmail.com>
2023-03-02 15:59:39 +0200

    push

--- pract/API/Program.cs
+++ pract/API/Program.cs
@@ -16,7 +16,7 @@ public class Program
         builder.Services.AddServices();
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
+        // Configure the HTTP request pipeline.//
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
